0fdeac4cd0de952f64b06908112fd2894601dd55
RajnandaniPawar <164873503+RajnandaniPawar@users.noreply.github.com>
2024-10-16 23:25:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 //complete this code
 class Person {
 	constructor(name, age) {
-        this._name = name;
-        this._age = age;
+        this.name = name;
+        this.age = age;
     }
 
     // Getter for the name
     get name() {
-        return this._name;
+        return this.name;
     }
 
     // Setter for the age
     set age(age) {
-        this._age = age;
+        this.age = age;
     }
 
     // Getter for the age (optional, if needed for accessing the age directly)
     get age() {
-        return this._age;
+        return this.age;
     }
 
 }
